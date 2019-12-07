afc597d2fdf0691505fc56648b7cf1c3adfb0b4c
lilao <lilao@163.com>
2019-12-07 19:57:12 +0800

    deploy to amax5, 7

---
+++ bin/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+if [[ -z "$1" ]]; then
+  remote_host="felix@dgx"
+else
+  remote_host="felix@$1"
+fi
+#
+remote_dir="~/"
+
+cd ..
+
+if [[ -z "$2" ]]; then
+    echo rsync -avz --no-links --exclude-from './bin/exclude.txt' $(pwd) $remote_host:$remote_dir
+    rsync -avz --no-links --exclude-from './bin/exclude.txt' $(pwd) $remote_host:$remote_dir
+else
+    rsync -avz --no-links $(pwd) $remote_host:$remote_dir
+fi
+
+date
+
+echo 'upload to:' $remote_host:$remote_dir
+echo '===================================='
+
+#rsync -av  ./output/0.70180553000.csv hdpsbp@ai-prd-07:/users/hdpsbp/felix/df_jf/output/
+
+
+#rsync -av hdpsbp@ai-prd-04:/users/hdpsbp/felix/kdd_bd   /apps/
+
+
+#rsync -av  ./input/tmp hdpsbp@ai-prd-07:/users/hdpsbp/felix/kdd_bd/input
+
+#rsync -av   ./output/sub/?.csv hdpsbp@ai-prd-07:/users/hdpsbp/felix/kdd_bd/output/sub
